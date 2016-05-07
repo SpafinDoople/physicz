3d4295ff7e0d497a64adc978f25432c1d026bece
Your Name <spafindoople@gmail.com>
2016-05-07 10:34:59 -0700

    added respawning

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+var keys = [];
+
 var world = new Box2D.b2World( new Box2D.b2Vec2(0.0, 300.0) );
 var debugDraw = getCanvasDebugDraw();
 var e_shapeBit = 0x0001;
@@ -10,8 +12,10 @@ world.SetDebugDraw(debugDraw);
 var iteration = 0;
 var timeStep = 1/60;
 
-var player = createCircle(300,0,10);
+var player = createCircle(20,400,10);
+
 console.log(player);
+initControls();
 function createCircle(x,y,r) {
   var bd = new Box2D.b2BodyDef();
   bd.set_type(Module.b2_dynamicBody);
@@ -52,6 +56,16 @@ function mainLoop(){
   context.fillRect(0,0,600,480);
   draw(context);
   iteration++;
+  if(player.GetPosition().get_y() > 500) {
+    player.SetTransform(new b2Vec2(20,400), 0);
+  }
+  //37-40 = left up right down
+  var speed = 100000;
+  if(keys[37]) player.ApplyForce(new Box2D.b2Vec2(-speed,0), player.GetWorldCenter());
+  if(keys[39]) player.ApplyForce(new Box2D.b2Vec2(speed,0), player.GetWorldCenter());
+  if(keys[40])player.ApplyForce(new Box2D.b2Vec2(0,speed), player.GetWorldCenter());
+  //console.log(player.GetLinearVelocity.get_x());
+  if(keys[38]) player.ApplyLinearImpulse(new b2Vec2(/*new Box2D.b2Vec2(player.GetLinearVelocity().get_x()*/0,-100000), player.GetWorldCenter());
 }
 
 function draw(context){
@@ -66,27 +80,26 @@ function mouseSquare(e) {
   createCircle(e.clientX,e.clientY,20);
 }
 
-function keyboardListener(e) {
-  //37-40 = left up right down
-  var key = e.keyCode;
-  var speed = 1000000;
-  if(key == 37) {
-    player.ApplyForce(new Box2D.b2Vec2(-speed,0), player.GetWorldCenter());
-  }
-  if(key == 38) {
-    player.ApplyForce(new Box2D.b2Vec2(0,-speed), player.GetWorldCenter());
-  }
-  if(key == 39) {
-    player.ApplyForce(new Box2D.b2Vec2(speed,0), player.GetWorldCenter());
-  }
-  if(key== 40) {
-    player.ApplyForce(new Box2D.b2Vec2(0,speed), player.GetWorldCenter());
+function initControls() {
+  for (var i = 0; i < 222; i++) {
+    keys.push(false);
   }
 }
 
-createRectangle(100,480,200,20); //bottome ground
-createRectangle(350,480,100,20);
-createRectangle(600,480,200,20);
+function keyUp(e) {
+  var key = e.keyCode;
+  keys[key] = false;
+}
+
+function keyDetect(e) {
+  var key = e.keyCode;
+  keys[key] = true;
+}
+
+
+createRectangle(100,480,200,50); //bottome ground
+createRectangle(350,480,100,50);
+createRectangle(600,480,200,50);
 //createGround(450,120,50,50); //right box
 //createGround(150,120,50,50); //left box
 //createGround(300,280,50,50); //center box
@@ -97,4 +110,5 @@ createRectangle(600,-50,600,50); //top wall
 
 setInterval(mainLoop, 1000/60);
 document.addEventListener("click", mouseSquare);
-document.addEventListener("keydown", keyboardListener);
+document.addEventListener("keydown", keyDetect);
+document.addEventListener("keyup", keyUp);
